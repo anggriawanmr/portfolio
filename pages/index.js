26a1ff6811f297664e06293179145d1eebc57b22
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-10-11 18:18:40 +0700

    Adding layout to work and index :sparkles:

--- pages/index.js
+++ pages/index.js
@@ -9,73 +9,76 @@ import {
 } from '@chakra-ui/react';
 import { ChevronRightIcon } from '@chakra-ui/icons';
 
+import Layout from '../components/layouts/article';
 import Section from '../components/section';
 import Paragraph from '../components/paragraph';
 import { BioSection, BioYear } from '../components/bio';
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Hello, I&apos;m a full-stack developer based in Indonesia!
-      </Box>
-
-      <Box display={{ xs: 'flex' }} mb={10}>
-        <Box flexGrow={1}></Box>
-        <Heading as="h2" variant="page-title">
-          Anggriawan Ridatama
-        </Heading>
-        <p>Web Developer and Experienced Customer Service Representative</p>
-      </Box>
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          About
-        </Heading>
-        <Paragraph>
-          Anggriawan is Web Developer and Experienced Customer Service
-          Representative, passionate on doing web design and learning full-stack
-          development, currently he designing website for{' '}
-          <NextLink href="https://www.wwpconstruction.com/">
-            <Link>Wahana Wira Persada</Link>
-          </NextLink>
-          .
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon />} coloraScheme="teal">
-              My Portfolio
-            </Button>
-          </NextLink>
+    <Layout>
+      <Container>
+        <Box
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
+          align="center"
+        >
+          Hello, I&apos;m a full-stack developer based in Indonesia!
         </Box>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1994</BioYear>
-          Born in Malang, Indonesia.
-        </BioSection>
-        <BioSection>
-          <BioYear>2016</BioYear>
-          Completed Bachelor Degree of Computer Science in BINUS University
-        </BioSection>
-        <BioSection>
-          <BioYear>2015</BioYear>
-          Worked at Keywords Studios as a Customer Service Representative
-        </BioSection>
-        <BioSection>
-          <BioYear>2022</BioYear>
-          Worked at Wahana Wira Persada as a Web Developer
-        </BioSection>
-      </Section>
-    </Container>
+
+        <Box display={{ xs: 'flex' }} mb={10}>
+          <Box flexGrow={1}></Box>
+          <Heading as="h2" variant="page-title">
+            Anggriawan Ridatama
+          </Heading>
+          <p>Web Developer and Experienced Customer Service Representative</p>
+        </Box>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            About
+          </Heading>
+          <Paragraph>
+            Anggriawan is Web Developer and Experienced Customer Service
+            Representative, passionate on doing web design and learning
+            full-stack development, currently he designing website for{' '}
+            <NextLink href="https://www.wwpconstruction.com/">
+              <Link>Wahana Wira Persada</Link>
+            </NextLink>
+            .
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} coloraScheme="teal">
+                My Portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1994</BioYear>
+            Born in Malang, Indonesia.
+          </BioSection>
+          <BioSection>
+            <BioYear>2016</BioYear>
+            Completed Bachelor Degree of Computer Science in BINUS University
+          </BioSection>
+          <BioSection>
+            <BioYear>2015</BioYear>
+            Worked at Keywords Studios as a Customer Service Representative
+          </BioSection>
+          <BioSection>
+            <BioYear>2022</BioYear>
+            Worked at Wahana Wira Persada as a Web Developer
+          </BioSection>
+        </Section>
+      </Container>
+    </Layout>
   );
 };
 export default Page;
--- pages/works.js
+++ pages/works.js
@@ -1,5 +1,6 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react';
 
+import Layout from '../components/layouts/article';
 import Section from '../components/section';
 import { WorkGridItem } from '../components/grid-item';
 import thumbnailDashboard from '../public/images/works/dashboard.png';
@@ -7,32 +8,34 @@ import thumbUniswapClone from '../public/images/works/uniswap-clone.png';
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={5} mt={5}>
-        Projects
-      </Heading>
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={5} mt={5}>
+          Projects
+        </Heading>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <WorkGridItem
-            id="dashboard"
-            title="Dashboard"
-            thumbnail={thumbnailDashboard}
-          >
-            A Dashboard application
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.1}>
-          <WorkGridItem
-            id="uniswap-clone"
-            title="Uniswap Clone"
-            thumbnail={thumbUniswapClone}
-          >
-            Uniswap Clone
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem
+              id="dashboard"
+              title="Dashboard"
+              thumbnail={thumbnailDashboard}
+            >
+              A Dashboard application
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.1}>
+            <WorkGridItem
+              id="uniswap-clone"
+              title="Uniswap Clone"
+              thumbnail={thumbUniswapClone}
+            >
+              Uniswap Clone
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   );
 };
 
