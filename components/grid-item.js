60cba26165624a569bf6dd6eeeffef8118f20787
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-11-08 15:26:03 +0700

    Adding chakra.js :spakles:

--- components/grid-item.js
+++ components/grid-item.js
@@ -4,7 +4,7 @@ import { Box, Text, LinkBox, LinkOverlay } from '@chakra-ui/react';
 import { Global } from '@emotion/react';
 
 export const GridItem = ({ children, href, title, thumbnail }) => (
-  <Box w="100%" align="center">
+  <Box w="100%" textAlign="center">
     <LinkBox cursor="pointer">
       <Image
         src={thumbnail}
@@ -14,7 +14,7 @@ export const GridItem = ({ children, href, title, thumbnail }) => (
         loading="lazy"
       />
       <LinkOverlay href={href} target="_blank">
-        <Text ml={2}>{title}</Text>
+        <Text mt={2}>{title}</Text>
       </LinkOverlay>
       <Text fontSize={14}>{children}</Text>
     </LinkBox>
@@ -42,12 +42,12 @@ export const WorkGridItem = ({ children, id, title, thumbnail }) => (
   </Box>
 );
 
-export const GridItemStyle = () => {
+export const GridItemStyle = () => (
   <Global
     styles={`
     .grid-item-thumbnail {
       border-radius: 12px;
     }
   `}
-  />;
-};
+  />
+);
